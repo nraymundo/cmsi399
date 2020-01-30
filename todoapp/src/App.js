1861d1a8482c494ac9f082c731dce92a811245f0
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-01-30 12:32:10 -0800

    Finished video 2

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
     todos: [
       { id: 1, content: "drink brews with the boys" },
-      { id: 2, content: "shoot up with the boys" },
+      { id: 2, content: "ball with the boys" },
       { id: 3, content: "run from 12" },
-      { id: 4, content: "hit a lick" },
       { id: 5, content: "get arrested" },
       { id: 6, content: "break out of jail" },
-      { id: 7, content: "drink brews with the boys" }
+      { id: 7, content: "drink brews again with the boys" }
     ]
   };
   deleteTodo = id => {
@@ -21,11 +21,19 @@ class App extends Component {
       todos
     });
   };
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    });
+  };
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
